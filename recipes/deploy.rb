c3b8cbc4304b932903c7afa92858d491a7276a65
Rama moorthy <rama.moorthy@razerzone.com>
2015-09-07 17:59:55 +0800

    as

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -8,7 +8,7 @@ node[:deploy].each do |application, deploy|
     Chef::Log.debug("Skipping opsworks_delayed_job::deploy application #{application} as it is not an Rails app")
     next
   end
-  Chef::Log.INFO("Running custom opsworks recipes")
+  Chef::Log.warn("Running custom opsworks recipes")
 
   opsworks_deploy_dir do
     user deploy[:user]
