67884ef4990e65b405a625d46910ad000809aed0
Rama moorthy <rama.moorthy@razerzone.com>
2015-09-07 14:40:44 +0800

    as

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -10,12 +10,12 @@ node[:deploy].each do |application, deploy|
   Chef::Log.info("--------------------------------------A")
   node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
   Chef::Log.info("--------------------------------------")
-  template "#{deploy[:deploy_to]}/shared/config/database1.yml" do
+  template "#{deploy[:deploy_to]}/shared/config/database.yml" do
     source "database.yml.erb"
     mode "0660"
     group deploy[:group]
     owner deploy[:user]
-    variables(:database => deploy[:database], :environment => deploy[:rails_env])
+    variables(:environment => deploy[:rails_env])
 
     notifies :run, resources(:execute => "restart Rails app #{application}")
 
